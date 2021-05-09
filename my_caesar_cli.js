98c1ad485ae3dee7d3bd284692cafbfe90b326b1
Artem Bazykin <artem.bazykin.ddi@gmail.com>
2021-05-09 21:21:41 +0300

    Add caesar algoritm. Refactor code

--- my_caesar_cli.js
+++ my_caesar_cli.js
@@ -15,10 +15,42 @@ const validationArgs = () => {
 
   program.parse(process.argv);
 
-  const options = program.opts();
+  const { action, shift, input, output } = program.opts();
 
-  if (!options.action) errorHandler(new Error('Missing required action args'));
-  if (!options.shift) errorHandler(new Error('Missing required shift args'));
+  if (!action) errorHandler(new Error('Missing required action args'));
+  if (!shift) errorHandler(new Error('Missing required shift args'));
+  if (!input && !output) {
+    const readline = require('readline');
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question('Please input phase: ', (answer) => {
+      const result = caesar(answer, action === 'encode' ? shift : -shift);
+      process.stderr.write(`Transformed phase: ${result}`);
+      rl.close();
+    });
+  }
+};
+
+const caesar = (input, shift) => {
+  if (shift < 0) shift += 26;
+  let output = '';
+  for (let i = 0; i < input.length; i++) {
+    let c = input[i];
+    if (c.match(/[a-z]/i)) {
+      const code = input.charCodeAt(i);
+      if (code >= 65 && code <= 90) {
+        c = String.fromCharCode(((code - 65 + +shift) % 26) + 65);
+      } else if (code >= 97 && code <= 122) {
+        c = String.fromCharCode(((code - 97 + +shift) % 26) + 97);
+      }
+    }
+    output += c;
+  }
+
+  return output;
 };
 
 validationArgs();
